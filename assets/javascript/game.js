01c4f9fa862530c8d0382daef626f4de6e1058c8
Jacob <jrnels10@gmail.com>
2018-06-15 20:18:29 -0700

    math only runs on new data

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,99 @@
 $(document).ready(function () {
-
     // Initialize Firebase
     var config = {
-        apiKey: "AIzaSyBOiHjYKn_QoqL5RLl219teUF9-DaB49gc",
-        authDomain: "hethere-51bee.firebaseapp.com",
-        databaseURL: "https://hethere-51bee.firebaseio.com",
-        projectId: "hethere-51bee",
-        storageBucket: "hethere-51bee.appspot.com",
-        messagingSenderId: "427607132711"
+        apiKey: "AIzaSyCBCZDX9SYItJQBMrgLg6y-SVPlPgA2JIk",
+        authDomain: "crazy-train-d797d.firebaseapp.com",
+        databaseURL: "https://crazy-train-d797d.firebaseio.com",
+        projectId: "crazy-train-d797d",
+        storageBucket: "crazy-train-d797d.appspot.com",
+        messagingSenderId: "792798205955"
     };
     firebase.initializeApp(config);
     var database = firebase.database();
-    var array = [];
+    var minTillTrain;
+    var nextTrain;
+    // var array = [];
 
     // To search for specific child within the database
-    var testID;
-    $("#test").on("click", function(){
-        database.ref("1").set({
-            test: "test2"
-        })
-    });
+    // var testID;
+    // $("#test").on("click", function(){
+    //     database.ref("1").set({
+    //         test: "test2"
+    //     })
+    // });
 
-    
-    $("#add-user").on("click", function (event) {
+
+    console.log("linked")
+    $("#add-train").on("click", function (event) {
+        console.log("click works")
         event.preventDefault();
         var name = $("#name-input").val().trim();
-        var role = $("#role-input").val().trim();
-        var startDate = $("#startdate-input").val().trim();
-        var rate = $("#rate-input").val().trim();
+        var destination = $("#dest-input").val().trim();
+        var firstTrain = $('#firstTrain-input').val().trim();
+        var frequency = $("#freq-input").val().trim();
 
         
         // console.log('Click works');
-        if (name && role && startDate && rate) {
+        // Solved Mathematically
+        // Test case 2:
+        // 16 - 00 = 16
+        // 16 % 7 = 2 (Modulus is the remainder)
+        // 7 - 2 = 5 minutes away
+        // 5 + 3:16 = 3:21
+
+        // First time is set back one year to ake sure it comes before current time
+        var firstTimeConverted = moment(firstTrain, 'hh:mm').subtract(1, "Years");
+        var currentTime = moment();
+        console.log("first time: " + moment(firstTimeConverted).format('hh:mm'));
+        console.log('Current time: ' + moment(currentTime).format("hh:mm"));
+
+        // difference in time 
+        var diffTime = moment().diff(moment(firstTimeConverted), 'minutes');
+        console.log('difference in time: ' + diffTime);
+
+        // time apart (remainder)
+        var remainder = diffTime % frequency;
+        console.log(remainder);
+
+        // minutes until next train
+        minTillTrain = frequency - remainder;
+        console.log("minutes till next train: " + minTillTrain);
+        
+        // what time the next train will arrive
+        nextTrain = moment(moment().add(minTillTrain, 'minutes')).format('hh:mm');
+        console.log("arrival time: " + moment(moment().add(minTillTrain, 'minutes')).format('hh:mm'));
+
+        if (name && destination && firstTrain && frequency) {
             database.ref(name).set({
                 name: name,
-                role: role,
-                startDate: startDate,
-                monthlyRate: rate
+                destination: destination,
+                firstTrain: firstTrain,
+                frequency: frequency,
+                // minutes: minTillTrain,
+                // nextTrain: nextTrain,
             });
+            console.log("next Train")
         } else {
             console.log("something is missing from the form");
         }
 
 
     });
-    database.ref().on("child_added", renderEmployees);//<--- look this up//likely updates when page loads and when a new child is added
-
-    function renderEmployees(snapshot) {
-        testID = snapshot.key;
-        array.push(testID);
-        console.log(array);
-        var employeeRow = $("<tr>");
+    database.ref().on("child_added", trainTimes);//<--- look this up//likely updates when page loads and when a new child is added
+    function trainTimes(snapshot) {
+        // testID = snapshot.key;
+        // array.push(testID);
+        // console.log(array);
+        var trainRow = $("<tr>");
         var name = $("<td>").text(snapshot.val().name);
-        var role = $('<td>').text(snapshot.val().role);
-        var startDate = $('<td>').text(snapshot.val().startDate);
-        var workTime = $('<td>').text(5);
-        var monthRate = $('<td>').text(snapshot.val().monthlyRate);
-        var cost = $('<td>').text(parseInt(workTime.text()) * parseInt(monthRate.text()));
-        employeeRow.append(name, role, startDate, workTime, monthRate, cost);
-        $("#tableBody").append(employeeRow);
+        var destination = $('<td>').text(snapshot.val().destination);
+        var frequency = $('<td>').text(snapshot.val().frequency);
+        var minutes = $('<td>').text(minTillTrain);
+        console.log("minutes: " + minutes);
+        var nextTime = $('<td>').text(nextTrain);
+        console.log("nextTime: " + nextTime);
+        trainRow.append(name, destination, frequency, minutes, nextTime);
+        $("#tableBody").append(trainRow);
     }
+    
 });
